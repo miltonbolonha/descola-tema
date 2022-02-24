e6da4b10acafb07e378162e6ee190ba6420e1ef5
miltonbolonha <miltonbolonha@gmail.com>
2022-02-24 15:18:35 -0300

    mobile menu added

--- src/modules/layout/components/MainMenu.js
+++ src/modules/layout/components/MainMenu.js
@@ -1,76 +1,19 @@
 import React from 'react'
-
+import { Link } from 'gatsby'
 const MainMenu = ({ wrapperRef, isVisibleClass }) => (
 	<nav ref={wrapperRef} className={' main-nav menu-state-' + isVisibleClass}>
 		<ul className="nav-menus">
-			<li className="nav-links">
-				<a className="nav-main-link" href="https://google.com/">
-					Ir Para <br className="desktop-only" />o Site
-				</a>
+			<li>
+				<a href="https://descola.org/cursos">Cursos</a>
 			</li>
-		</ul>
-
-		<ul className="nav-menus">
-			<li className="nav-links">
-				<a className="nav-main-link" href="https://google.com/quem-somos/">
-					Quem Somos
-				</a>
+			<li>
+				<a href="https://descola.org/sobre">O Que é a Descola?</a>
 			</li>
-		</ul>
-
-		<ul className="nav-menus">
-			<li className="nav-links">
-				<a className="nav-main-link" href="https://google.com/viver-bem/">
-					Viver Bem
-				</a>
+			<li>
+				<a href="https://descola.org/empresas">Para empresas</a>
 			</li>
-		</ul>
-		<ul className="nav-menus">
-			<li className="nav-links">
-				<a className="nav-main-link" href="https://google.com/fique-ligado/">
-					Fique Ligado
-				</a>
-			</li>
-		</ul>
-		<ul className="nav-menus">
-			<li className="nav-links">
-				<a
-					className="nav-main-link"
-					href="https://google.com/profissional-e-gestor-de-saude/"
-				>
-					Profissional e Gestor
-				</a>
-			</li>
-		</ul>
-		<ul className="nav-menus">
-			<li className="nav-links">
-				<a className="nav-main-link" href="https://google.com/faca-parte/">
-					Faça Parte
-				</a>
-			</li>
-		</ul>
-		<ul className="nav-menus">
-			<li className="nav-links">
-				<a className="nav-main-link" href="https://google.com/em-dia/">
-					Em Dia
-				</a>
-			</li>
-		</ul>
-		<ul className="nav-menus">
-			<li className="nav-links">
-				<a
-					className="nav-main-link"
-					href="https://google.com/faca-parte/faca-sua-doacao/"
-				>
-					Doações
-				</a>
-			</li>
-		</ul>
-		<ul className="nav-menus">
-			<li className="nav-links">
-				<a className="nav-main-link" href="https://google.com/contatos/">
-					Contatos
-				</a>
+			<li>
+				<Link to="/">Blog</Link>
 			</li>
 		</ul>
 	</nav>
